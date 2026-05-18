3cf20ee49d3f32124b8b8444cb2fd970aa415ad7
ahhglass <prochanovangel@gmail.com>
2026-05-18 13:55:35 +0300

    Update Text.svelte

--- src/lib/icons/Text.svelte
+++ src/lib/icons/Text.svelte
@@ -2,13 +2,14 @@
 	xmlns="http://www.w3.org/2000/svg"
 	width="24"
 	height="24"
+	viewBox="0 0 24 24"
 	fill="none"
 	stroke="currentColor"
 	stroke-width="1.5"
 	stroke-linecap="round"
+	stroke-linejoin="round"
+	class="icon icon-tabler icons-tabler-outline icon-tabler-ballpen"
+	><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14 6l7 7l-4 4" /><path
+		d="M5.828 18.172a2.828 2.828 0 0 0 4 0l10.586 -10.586a2 2 0 0 0 0 -2.829l-1.171 -1.171a2 2 0 0 0 -2.829 0l-10.586 10.586a2.828 2.828 0 0 0 0 4"
+	/><path d="M4 20l1.768 -1.768" /></svg
 >
-	<path d="M15 21.001H9" stroke-linejoin="round" />
-	<path
-		d="M12 3.00001V21.0008M12 3.00001C13.3874 3.00001 15.1695 3.03055 16.5884 3.17649C17.1885 3.2382 17.4886 3.26906 17.7541 3.37791C18.3066 3.60429 18.7518 4.10063 18.9194 4.67681C19 4.95382 19 5.26992 19 5.90215M12 3.00001C10.6126 3.00001 8.83047 3.03055 7.41161 3.17649C6.8115 3.2382 6.51144 3.26906 6.24586 3.37791C5.69344 3.60429 5.24816 4.10063 5.08057 4.67681C5 4.95382 5 5.26992 5 5.90215"
-	/>
-</svg>
